f889fd0994502ad77ba9235e89c43874fa160e27
2ombe <jtuombe@gmail.com>
2025-09-24 00:34:21 +0200

    save

--- backend/index.js
+++ backend/index.js
@@ -41,15 +41,11 @@ app.use('/api/suppliers', supplierRoutes);
 app.use('/api/plants', processingPlantRoutes);
 app.use('/api/transactions', transactionRoutes);
 // Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  // First, serve static files from the build directory
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  
-  // Then, handle all other routes by serving index.html
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
